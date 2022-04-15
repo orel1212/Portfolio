567873de91c5a5be31c2606ee4dfaa6ba0917cf2
Orel Lavie <orel1212356@gmail.com>
2022-04-15 10:27:03 +0300

    removed etc

--- CPP/SweetShop/sources/Cookielida.cpp
+++
@@ -1,9 +0,0 @@
-
-#include"Cookielida.h"
-Cookielida::Cookielida(const Taste& icecream, const Cookies& cookie) :Sweetitem(), Icecream(icecream, 1, IcecreamPrice), Cookie(cookie,2,CookiePrice)
-{
-}
-float Cookielida::GetPrice()
-{
-	return ((CookiePrice * 2 + IcecreamPrice) * 1.5);
-}
